2393a744dedcd6093c2fa04d734e585b295a959c
Dmitry Vislov <vdv73rus@gmail.com>
2014-10-28 08:09:52 +0300

    Content metro-ui grid added.

--- app/coffee/application.js.coffee
+++ app/coffee/application.js.coffee
@@ -1,1 +1,13 @@
-console.log 'This is coffee script success test!'
+$(window).resize ->
+  getMetroItemSize(5)
+  getMetroItemSize(8)
+
+$(document).ready ->
+  getMetroItemSize(5)
+  getMetroItemSize(8)
+
+getMetroItemSize = (num) ->
+  boxItemSize = Math.round(parseInt($('.metro-grid-' + num + ' .metro-row').width()) / num - 8)
+  console.log(boxItemSize);
+  $('.metro-grid-' + num + ' .metro-row').css
+    height: boxItemSize + 'px'
--- app/compile/js/application.js
+++ app/compile/js/application.js
@@ -1,4 +1,23 @@
 (function() {
-  console.log('This is coffee script success test!');
+  var getMetroItemSize;
+
+  $(window).resize(function() {
+    getMetroItemSize(5);
+    return getMetroItemSize(8);
+  });
+
+  $(document).ready(function() {
+    getMetroItemSize(5);
+    return getMetroItemSize(8);
+  });
+
+  getMetroItemSize = function(num) {
+    var boxItemSize;
+    boxItemSize = Math.round(parseInt($('.metro-grid-' + num + ' .metro-row').width()) / num - 8);
+    console.log(boxItemSize);
+    return $('.metro-grid-' + num + ' .metro-row').css({
+      height: boxItemSize + 'px'
+    });
+  };
 
 }).call(this);
