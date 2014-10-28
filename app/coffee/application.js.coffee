f428f004e50a9895f55bc8411b3d2fbfb3403f2b
Dmitry Vislov <vdv73rus@gmail.com>
2014-10-28 08:57:02 +0300

    Sticky nav + nav metro colored grid added.

--- app/coffee/application.js.coffee
+++ app/coffee/application.js.coffee
@@ -6,8 +6,13 @@ $(document).ready ->
   getMetroItemSize(5)
   getMetroItemSize(8)
 
+window.onscroll = ->
+  if (window.pageYOffset > 204)
+    $('.top-nav').addClass('sticky')
+  else
+    $('.top-nav').removeClass('sticky')
+
 getMetroItemSize = (num) ->
   boxItemSize = Math.round(parseInt($('.metro-grid-' + num + ' .metro-row').width()) / num - 8)
-  console.log(boxItemSize);
   $('.metro-grid-' + num + ' .metro-row').css
     height: boxItemSize + 'px'
--- app/compile/js/application.js
+++ app/compile/js/application.js
@@ -11,10 +11,17 @@
     return getMetroItemSize(8);
   });
 
+  window.onscroll = function() {
+    if (window.pageYOffset > 204) {
+      return $('.top-nav').addClass('sticky');
+    } else {
+      return $('.top-nav').removeClass('sticky');
+    }
+  };
+
   getMetroItemSize = function(num) {
     var boxItemSize;
     boxItemSize = Math.round(parseInt($('.metro-grid-' + num + ' .metro-row').width()) / num - 8);
-    console.log(boxItemSize);
     return $('.metro-grid-' + num + ' .metro-row').css({
       height: boxItemSize + 'px'
     });
