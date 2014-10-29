64d24bef26c3502cfaf13d54de924f503f4a40b8
Dmitry Vislov <vdv73rus@gmail.com>
2014-10-29 08:17:29 +0300

    Show top submenu onClick instead onHover.

--- app/coffee/application.js.coffee
+++ app/coffee/application.js.coffee
@@ -6,13 +6,28 @@ $(document).ready ->
   getMetroItemSize(5)
   getMetroItemSize(8)
 
+getMetroItemSize = (num) ->
+  boxItemSize = Math.round(parseInt($('.metro-grid-' + num + ' .metro-row').width()) / num - 8)
+  $('.metro-grid-' + num + ' .metro-row').css
+    height: boxItemSize + 'px'
+
+# stick menu to top
 window.onscroll = ->
   if (window.pageYOffset > 204)
     $('.top-nav').addClass('sticky')
   else
     $('.top-nav').removeClass('sticky')
 
-getMetroItemSize = (num) ->
-  boxItemSize = Math.round(parseInt($('.metro-grid-' + num + ' .metro-row').width()) / num - 8)
-  $('.metro-grid-' + num + ' .metro-row').css
-    height: boxItemSize + 'px'
+# open top menu
+$(document).ready ->
+  $('.top-nav-item-link').click ->
+
+    if ($(this).parent('li').hasClass("top-nav-item-active"))
+      $(".submenu").removeClass("active")
+      $(".top-nav-item-active").removeClass("top-nav-item-active")
+    else
+      $(".submenu").removeClass("active")
+      $(".top-nav-item-active").removeClass("top-nav-item-active")
+      $(this).parent('li').toggleClass("top-nav-item-active")
+      $(this).next(".submenu").addClass("active")
+
--- app/compile/js/application.js
+++ app/compile/js/application.js
@@ -11,6 +11,14 @@
     return getMetroItemSize(8);
   });
 
+  getMetroItemSize = function(num) {
+    var boxItemSize;
+    boxItemSize = Math.round(parseInt($('.metro-grid-' + num + ' .metro-row').width()) / num - 8);
+    return $('.metro-grid-' + num + ' .metro-row').css({
+      height: boxItemSize + 'px'
+    });
+  };
+
   window.onscroll = function() {
     if (window.pageYOffset > 204) {
       return $('.top-nav').addClass('sticky');
@@ -19,12 +27,18 @@
     }
   };
 
-  getMetroItemSize = function(num) {
-    var boxItemSize;
-    boxItemSize = Math.round(parseInt($('.metro-grid-' + num + ' .metro-row').width()) / num - 8);
-    return $('.metro-grid-' + num + ' .metro-row').css({
-      height: boxItemSize + 'px'
+  $(document).ready(function() {
+    return $('.top-nav-item-link').click(function() {
+      if ($(this).parent('li').hasClass("top-nav-item-active")) {
+        $(".submenu").removeClass("active");
+        return $(".top-nav-item-active").removeClass("top-nav-item-active");
+      } else {
+        $(".submenu").removeClass("active");
+        $(".top-nav-item-active").removeClass("top-nav-item-active");
+        $(this).parent('li').toggleClass("top-nav-item-active");
+        return $(this).next(".submenu").addClass("active");
+      }
     });
-  };
+  });
 
 }).call(this);
